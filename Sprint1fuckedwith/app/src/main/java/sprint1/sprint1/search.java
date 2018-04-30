755374b3529732f1a374246bd1ec682bc7967b4c
John Foster <fosterj5@RAMS.adp.vcu.edu>
2018-04-30 10:45:07 -0400

    search doesnt work

--- Sprint1fuckedwith/app/src/main/java/sprint1/sprint1/search.java
+++ Sprint1fuckedwith/app/src/main/java/sprint1/sprint1/search.java
@@ -3,21 +3,36 @@ package sprint1.sprint1;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 /**
  * Created by stwodaham on 4/3/2018.
  */
 
 public class search extends Activity {
+    private static String name;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_layout);
     }
 
+    public void userData(){
+        EditText et1 = (EditText) findViewById(R.id.editTextSearch1);
+        name = et1.getText().toString();
+        Log.d("FLAG", name);
+
+        final Intent intent = new Intent(search.this, searchedProduct.class);
+        intent.putExtra("Name of product", name);
+
+    }
+
     public void clickSearchWithinSearch(View v) {
         if(v.getId() == R.id.searchButton) {
             Intent i = new Intent(search.this, searchedProduct.class);
+            userData();
             startActivity(i);
         }
     }
--- Sprint1fuckedwith/app/src/main/java/sprint1/sprint1/searchedProduct.java
+++ Sprint1fuckedwith/app/src/main/java/sprint1/sprint1/searchedProduct.java
@@ -4,15 +4,38 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by stwodaham on 4/3/2018.
  */
 
 public class searchedProduct extends Activity {
+    private ListView mainListView;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.searched_product_layout);
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("Name of product");
+
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference singleName = database.getReference(name);
+
+        final List<String> listNames = new ArrayList<String>();
+        final ArrayAdapter<String> adapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listNames);
+
+        adapter.add(singleName.toString());
+        mainListView = (ListView) findViewById(R.id.searchList);
+        mainListView.setAdapter(adapter);
     }
 
     public void clickHomeWithinSearch(View v) {
